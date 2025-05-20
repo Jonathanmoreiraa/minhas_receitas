4c73dca22308ce00b4c9129e4a61e581a6a7e4e0
Vinícius Pereira <vinicius.gpereira03@gmail.com>
2025-05-19 22:00:43 -0300

    Criado componente de botão

---
+++ lib/presentation/widgets/custom_button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String label;
+  final VoidCallback onPressed;
+
+  const CustomButton({
+    Key? key,
+    required this.label,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Color.fromARGB(255, 255, 210, 161),
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8)
+        )
+      ),
+      onPressed: onPressed,
+      child: Text(
+        label,
+        style: const TextStyle(
+            color: Color.fromARGB(255, 126, 99, 76), 
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+            
+          ),
+      ),
+    );
+  }
+}
