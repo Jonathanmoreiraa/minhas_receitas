eaee63e6de398fe5c8d5f43a79d99a2832b4cba1
Vinícius Pereira <vinicius.gpereira03@gmail.com>
2025-05-23 23:17:57 -0300

    Criadas funções para banco offline sqlite

---
+++ lib/data/repositories/receita_repository.dart
@@ -0,0 +1,47 @@
+import '../models/receita.dart';
+import '../database/db_helper.dart';
+
+class ReceitaRepository {
+  Future<void> insertReceita(Receita receita) async {
+    final db = await DBHelper.database;
+    await db.insert('receitas', receita.toMap());
+  }
+
+  Future<List<Receita>> getReceitas() async {
+    final db = await DBHelper.database;
+    final result = await db.query('receitas');
+    return result.map((map) => Receita.fromMap(map)).toList();
+  }
+
+  Future<Receita?> getReceitaById(int id) async {
+    final db = await DBHelper.database;
+    final result = await db.query(
+      'receitas',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+    if (result.isNotEmpty) {
+      return Receita.fromMap(result.first);
+    }
+    return null;
+  }
+
+  Future<void> updateReceita(Receita receita) async {
+    final db = await DBHelper.database;
+    await db.update(
+      'receitas',
+      receita.toMap(),
+      where: 'id = ?',
+      whereArgs: [receita.id],
+    );
+  }
+
+  Future<void> deleteReceita(int id) async {
+    final db = await DBHelper.database;
+    await db.delete(
+      'receitas',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+}
